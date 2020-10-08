5db674a67bbd7502f5c6dc99bc60083dcfe86537
jonbae <jonbae61596@gmail.com>
2020-10-08 00:30:44 -0400

    add markdown editor

--- zk_frontend/src/editor/Editor.tsx
+++ zk_frontend/src/editor/Editor.tsx
@@ -1,18 +1,27 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import MarkdownEditor from "rich-markdown-editor";
+import debounce from "lodash/debounce";
 
 const Editor: React.FC = () => {
   const [title, setTitle] = useState("");
 
+  const handleChange = debounce((value) => {
+    const text = value();
+    console.log(text);
+    localStorage.setItem("saved", text);
+  }, 250);
+
   return (
     <>
+      <h1>editor page</h1>
+      <Link to="/home"> back to home page</Link>
       <input
         type="text"
         onChange={(e) => setTitle(e.currentTarget.value)}
         value={title}
       />
-      <h1>editor page</h1>
-      <Link to="/home"> back to home page</Link>
+      <MarkdownEditor onChange={handleChange} defaultValue={"hello world!"} />
     </>
   );
 };
